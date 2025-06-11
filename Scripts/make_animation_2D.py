933aaa1c962088ec0e597b393f00dee06a5b16c5
Mandriantso <mandriantso@ensc.fr>
2025-06-11 13:29:19 +0200

    update size and alpha of neurons over 50 frames

--- Scripts/make_animation_2D.py
+++ Scripts/make_animation_2D.py
@@ -5,25 +5,16 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(script_dir)
 sys.path.append(parent_dir)
 
-import time
-from datetime import datetime
-import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from tqdm.autonotebook import tqdm
 
-from neuron import h
 from Scripts.svg import *
-from Scripts.Network import *
-from Scripts.utilities import *
 from Scripts.anatomy import *
-from Scripts.input import *
 from Model import settings
 import parameters
-import matplotlib.pyplot as plt
-from matplotlib import colors
 
-import numpy as np
-import random
-from collections import OrderedDict
-import matplotlib.animation as animation
 
 results_dir = os.path.join(parent_dir, 'extra_stim_cartesian_results', '2025_06_06 11H31M42 2_mA new coords - inner stim - 100 ms')
 data_dir = os.path.join(results_dir, 'data')
@@ -110,29 +101,35 @@ c1, d1, idx = external_shape(new_points, thickness/2)
 
 shape_length, arcs_lengths = get_shape_length(new_points)
 
-# upper_line = []
-# for i in range(101):
-#     x, y = get_flattened_coordinates(i*0.01, 1.0, shape_length, thickness)
-#     upper_line.extend(np.array([[x,y]]))
-# upper_line = np.array(upper_line).reshape(-1,2)
-
-# lower_line = upper_line.copy()
-# lower_line[:,1] = 0.0
-
 color = ['blue', 'brown', 'red', 'purple', 'green']
 cell_colors = [[0., 0., 1., 1.], [1., 0., 0., 1.], [1., 0.54901961, 0., 1.], [0., 0., 0., 1.]]
 
+min_size = 30
+max_size = 400
+
 # initial states
-pyr_facecolors = [[0., 0., 0., 0.]] * len(pyr_coords)
+pyr_facecolors = [cell_colors[0]] * len(pyr_coords)
+# pyr_facecolors = [[1., 1., 1., 1.]] * len(pyr_coords)
 pyr_facecolors = np.array(pyr_facecolors)
 
-bc_facecolors = [[0., 0., 0., 0.]] * len(bc_coords)
+bc_facecolors = [cell_colors[1]] * len(bc_coords)
+# bc_facecolors = [[1., 1., 1., 1.]] * len(bc_coords)
 bc_facecolors = np.array(bc_facecolors)
 
-olm_facecolors = [[0., 0., 0., 0.]] * len(olm_coords)
+olm_facecolors = [cell_colors[2]] * len(olm_coords)
+# olm_facecolors = [[1., 1., 1., 1.]] * len(olm_coords)
 olm_facecolors = np.array(olm_facecolors)
 
-elec_facecolors = np.array([[0., 0., 0., 0.]])
+pyr_sizes = [1.] * len(pyr_coords)
+pyr_sizes = np.array(pyr_sizes)
+
+bc_sizes = [1.] * len(bc_coords)
+bc_sizes = np.array(bc_sizes)
+
+olm_sizes = [1.] * len(olm_coords)
+olm_sizes = np.array(olm_sizes)
+
+elec_facecolors = np.array([[1., 1., 1., 1.]])
 
 ## set figure
 fig, ax = plt.subplots(figsize=(15, 15))
@@ -168,10 +165,18 @@ for idx in range(len(olm_spk_t)):
 
 elec_raster[int(stim_on):int(stim_on+stim_dur),0] = True
 
-pyr_scat = ax.scatter(pyr_coords[:,0], pyr_coords[:,1], s=200., edgecolors=cell_colors[0], facecolors=pyr_facecolors)
-bc_scat = ax.scatter(bc_coords[:,0], bc_coords[:,1], s=200, edgecolors=cell_colors[1], facecolors=bc_facecolors)
-olm_scat = ax.scatter(olm_coords[:,0], olm_coords[:,1], s=200, edgecolors=cell_colors[2], facecolors=olm_facecolors)
+pyr_scat_fix = ax.scatter(pyr_coords[:,0], pyr_coords[:,1], s=1, edgecolors=cell_colors[0], facecolors=cell_colors[0])
+bc_scat_fix = ax.scatter(bc_coords[:,0], bc_coords[:,1], s=1, edgecolors=cell_colors[1], facecolors=cell_colors[1])
+olm_scat_fix = ax.scatter(olm_coords[:,0], olm_coords[:,1], s=1, edgecolors=cell_colors[2], facecolors=cell_colors[2])
+
+pyr_scat = ax.scatter(pyr_coords[:,0], pyr_coords[:,1], s=pyr_sizes, edgecolors=pyr_facecolors, facecolors=pyr_facecolors)
+bc_scat = ax.scatter(bc_coords[:,0], bc_coords[:,1], s=bc_sizes, edgecolors=bc_facecolors, facecolors=bc_facecolors)
+olm_scat = ax.scatter(olm_coords[:,0], olm_coords[:,1], s=olm_sizes, edgecolors=olm_facecolors, facecolors=olm_facecolors)
 elec_scat = ax.scatter(settings.stim_pos[0], settings.stim_pos[1], s=400, edgecolors=cell_colors[3], facecolors=elec_facecolors)
+
+pyr_scat.set_alpha = 0.
+bc_scat.set_alpha = 0.
+olm_scat.set_alpha = 0.
 
 check_text = ax.text(settings.stim_pos[0] - 2500, settings.stim_pos[1] + 2500, 'time = 0 ms', fontsize=12)
 
@@ -186,8 +191,37 @@ def update(frame):
             elec_facecolors[0] = cell_colors[3] 
             elec_scat.set_facecolors(elec_facecolors)
     else:
-        elec_facecolors[0] = [0., 0., 0., 0.]
+        elec_facecolors[0] = [1., 1., 1., 1.]
         elec_scat.set_facecolors(elec_facecolors)
+
+    ## sizes
+    # pyr_sizes = pyr_scat.get_sizes()
+    # bc_sizes = pyr_scat.get_sizes()
+    # olm_sizes = pyr_scat.get_sizes()
+
+    index_pyr = np.argwhere(pyr_sizes[(pyr_sizes > 1)] < max_size)
+    index_bc = np.argwhere(bc_sizes[(bc_sizes > 1)] < max_size)
+    index_olm = np.argwhere(olm_sizes[(olm_sizes > 1)] < max_size)
+
+    pyr_sizes[index_pyr] += (max_size - min_size)/50
+    bc_sizes[index_bc] += (max_size - min_size)/50
+    olm_sizes[index_olm] += (max_size - min_size)/50
+
+    pyr_facecolors[index_pyr,3] = np.maximum(0, pyr_facecolors[index_pyr,3] - 1/50)
+    bc_facecolors[index_bc,3] = np.maximum(0, bc_facecolors[index_bc,3] - 1/50)
+    olm_facecolors[index_olm,3] = np.maximum(0, olm_facecolors[index_olm,3] - 1/50)
+
+    index_pyr = pyr_sizes == max_size
+    index_bc = bc_sizes == max_size
+    index_olm = olm_sizes == max_size
+
+    pyr_sizes[index_pyr] = 1
+    bc_sizes[index_bc] = 1
+    olm_sizes[index_olm] = 1
+
+    pyr_facecolors[index_pyr,3] = 0.
+    bc_facecolors[index_bc,3] = 0.
+    olm_facecolors[index_olm,3] = 0.
 
     # update neurons
     # get the indexes of the neurons active during frame
@@ -195,31 +229,44 @@ def update(frame):
     # put everything to inactive first
     # put the updated to active
     # pyramidal cells
-    pyr_facecolors[:] =  [0., 0., 0., 0.]
+    # pyr_facecolors[:,3] =  0.
+    # pyr_facecolors[:] =  [1., 1., 1., 1.]
     index_active_pyr = np.argwhere(pyr_spk_t.astype(int) == frame * 1)
     if len(index_active_pyr) > 0:
         id_active_pyr = pyr_spk_id[index_active_pyr]
-    
-        pyr_facecolors[id_active_pyr.astype(int)] = cell_colors[0] 
+        pyr_facecolors[id_active_pyr.astype(int),3] =  1.
+        pyr_sizes[id_active_pyr.astype(int)] = min_size
+        # pyr_facecolors[id_active_pyr.astype(int)] = cell_colors[0] 
     pyr_scat.set_facecolors(pyr_facecolors)
+    pyr_scat.set_edgecolors(pyr_facecolors)
     
     # bc cells
-    bc_facecolors[:] =  [0., 0., 0., 0.]
+    # bc_facecolors[:,3] =  0.
+    # bc_facecolors[:] =  [1., 1., 1., 1.]
     index_active_bc = np.argwhere(bc_spk_t.astype(int) == frame * 1)
     if len(index_active_bc) > 0:
         id_active_bc = bc_spk_id[index_active_bc]
-    
-        bc_facecolors[id_active_bc.astype(int) - len(pyr_coords)] = cell_colors[1] 
+        bc_facecolors[id_active_bc.astype(int) - len(pyr_coords),3] =  1.
+        bc_sizes[id_active_bc.astype(int) - len(pyr_coords)] = min_size
+        # bc_facecolors[id_active_bc.astype(int) - len(pyr_coords)] = cell_colors[1] 
     bc_scat.set_facecolors(bc_facecolors)
+    bc_scat.set_edgecolors(bc_facecolors)
 
     # olm cells
-    olm_facecolors[:] =  [0., 0., 0., 0.]
+    # olm_facecolors[:,3] =  0.
+    # olm_facecolors[:] =  [1., 1., 1., 1.]
     index_active_olm = np.argwhere(olm_spk_t.astype(int) == frame * 1)
     if len(index_active_olm) > 0:
         id_active_olm = olm_spk_id[index_active_olm]
-    
-        olm_facecolors[id_active_olm.astype(int) - len(pyr_coords) - len(bc_coords)] = cell_colors[2] 
+        olm_facecolors[id_active_olm.astype(int) - len(pyr_coords) - len(bc_coords),3] =  1.
+        olm_sizes[id_active_olm.astype(int) - len(pyr_coords) - len(bc_coords)] = min_size
+        # olm_facecolors[id_active_olm.astype(int) - len(pyr_coords) - len(bc_coords)] = cell_colors[2] 
     olm_scat.set_facecolors(olm_facecolors)
+    olm_scat.set_edgecolors(olm_facecolors)
+
+    pyr_scat.set_sizes(pyr_sizes)
+    bc_scat.set_sizes(bc_sizes)
+    olm_scat.set_sizes(olm_sizes)
 
     return elec_scat, check_text, pyr_scat, bc_scat, olm_scat,
 
@@ -228,7 +275,9 @@ ani = animation.FuncAnimation(fig=fig, func=update, frames=int(settings.duration
 # ani.save(os.path.join(results_dir, 'activation_20fps.gif'), fps=20)
 # Writer = animation.writers['ffmpeg']
 # writer = Writer(fps=20)
+bar = tqdm(total=int(settings.duration/1), file=sys.stdout)
 FFwriter = animation.FFMpegWriter(fps=20)
-ani.save(os.path.join(results_dir, 'activation_20fps.mp4'), writer=FFwriter)
+ani.save(os.path.join(results_dir, 'activation_alpha_size_20fps.mp4'), writer=FFwriter, dpi=300, progress_callback = lambda i, n: bar.update(1))
+bar.close()
 plt.show()
 
