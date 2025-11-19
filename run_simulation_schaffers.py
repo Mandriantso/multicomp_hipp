2ba44f899235d5ad0f0d2b136bc0295e3e61e627
Mandriantso <mandriantso@ensc.fr>
2025-11-19 11:51:00 +0100

    try w_sca scaled by 3 and amp increased to 0.2 nA

--- run_simulation_schaffers.py
+++ run_simulation_schaffers.py
@@ -745,7 +745,7 @@ for cell_ in ca1_pyr_cells: # to | from
             mt_.select("Exp2Syn")
             pp = mt_.pp_begin(sec=target_sec)
             nc_ = pc.gid_connect(pregid, pp)
-            nc_.weight[0] = settings.w_CA3_CA1[0]*5
+            nc_.weight[0] = settings.w_CA3_CA1[0]*3
             nc_.threshold = settings.syn_threshold
             nc_.delay = settings.syn_delay
             cell_._ncs.append(nc_)
@@ -805,7 +805,7 @@ if rank == 0:
     sys.stdout.flush()
 
 duration = 5000
-amp = 0.02#0.005 #0 #6.0
+amp = 0.2#0.005 #0 #6.0
 delay = 1000
 
 # osc_amp = h.Vector()
@@ -1100,7 +1100,7 @@ if rank == 0:
         f.write("remark :\n")
         f.write("J'ai utilisé une nouvelle distribution des collatéraux de Schaffer\n")
         f.write("Comme ils ont l'air mieux distribués dans l'espace, j'ai enlevé la contrainte qui consister à avoir 3 connexions de Schaffer max par pyr")
-        f.write("Je refais les expériences w_sca * 5 et offset (0, 50) pour un courant oscillatoire à 0.02nA")
+        f.write("Je refais les expériences w_sca * 3 et offset (0, 50) pour un courant oscillatoire à 0.2nA")
         f.write("Collatéraux avec le modèle de McIntyre")
         f.write("- no Pyr - Pyr connections\n")
         f.write("- pyr-bc weights used for sca-pyr connections\n")
@@ -1111,7 +1111,7 @@ if rank == 0:
         f.write("BC - BC weight : {}\n".format(settings.w_CA1[1][1]))
         f.write("OLM - Pyr weight : {}\n".format(settings.w_CA1[2][0]))
         f.write("Pyr - OLM weight : {}\n".format(settings.w_CA1[0][2]))
-        f.write("\nsca - pyr weight : {}\n".format(settings.w_CA3_CA1[0]*5))
+        f.write("\nsca - pyr weight : {}\n".format(settings.w_CA3_CA1[0]*3))
 
         f.write("\n\nSimulation results\n")
         f.write("-------------------------\n")
