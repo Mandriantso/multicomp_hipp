528e29a36e1d4c11a8a9fd9fb1df8fe4b50a5aba
Mandriantso <mandriantso@ensc.fr>
2026-02-11 15:04:04 +0100

    test perpendicular bipolar biphasic stim

--- run_simulation_schaffers.py
+++ run_simulation_schaffers.py
@@ -105,7 +105,7 @@ parser.add_argument('-p', '--parameters',
                     nargs='?',
                     metavar='-p',
                     type=str,
-                    default=os.path.join('configs', 'fixed_parameters_bipolar_stim_par_CA1_biphasic_train_pulses.json'), 
+                    default=os.path.join('configs', 'fixed_parameters_bipolar_stim_perp_CA1_biphasic_train_pulses.json'), 
                     help='Parameters file (json format)')
 
 parser.add_argument('-sd', '--save_dir',
@@ -160,7 +160,7 @@ if not os.path.isdir(dirs['results']) and rank == 0:
     sys.stdout.flush()
     os.makedirs(dirs['results'])
 
-dirs['save_dir'] = os.path.join(dirs['results'], datetime.now().strftime(f"%Y_%m_%d %HH%MM%S test train pulses 50Hz 0.300ms 1.5mA - bipolar par CA1"))
+dirs['save_dir'] = os.path.join(dirs['results'], datetime.now().strftime(f"%Y_%m_%d %HH%MM%S test train pulses 50Hz 0.300ms 1.5mA - bipolar perp CA1"))
 if not os.path.isdir(dirs['save_dir']) and rank == 0:
     print('[+] Creating directory', dirs['save_dir'])
     sys.stdout.flush()
@@ -1176,7 +1176,7 @@ if rank == 0:
     save_all_nodes_data(os.path.join(dirs['data'], 'CA3_all_nodes_i'), np.array(t_vec), merged_data_nodes_i)
 
     if settings.stim_status:
-        if settings.stim_type == "bipolar":
+        if settings.stim_electrode == "bipolar":
             stim_loc_pyr0 = np.sqrt((ca1_pyr_coords[:,0] - settings.stim_pos[0][0])**2+(ca1_pyr_coords[:,1] - settings.stim_pos[0][1])**2).argmin()
             stim_loc_pyr1 = np.sqrt((ca1_pyr_coords[:,0] - settings.stim_pos[1][0])**2+(ca1_pyr_coords[:,1] - settings.stim_pos[1][1])**2).argmin()
             stim_loc_bc0 = np.sqrt((ca1_bc_coords[:,0] - settings.stim_pos[0][0])**2+(ca1_bc_coords[:,1] - settings.stim_pos[0][1])**2).argmin()+len(ca1_pyr_coords)
