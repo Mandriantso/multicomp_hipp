a47ab5aef32557fb834438888a0fdc5b9f7c464a
Mandriantso <mandriantso@ensc.fr>
2026-02-12 17:29:33 +0100

    bipolar biphasic at 50Hz with interphase and 6mA amp

--- parameters.py
+++ parameters.py
@@ -267,7 +267,7 @@ _data = {
         "I": 6, # (mA) stimulation amplitude
         "pulse_width": 0.300, #ms
         "interphase": 0.100, # ms
-        "frequency": 5 # Hz
+        "frequency": 50 # Hz
     },
     "simulation": {
         "duration": 5000.0, # (ms)
